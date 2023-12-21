db527924b3301e125620fb3b31fbd285491dd81b
AntonZinchenko32 <zinchenko309@gmail.com>
2023-12-21 04:12:51 +0200

    add errorHandler to resendVerifLetter func

--- controllers/auth/userResendVerificLetter.js
+++ controllers/auth/userResendVerificLetter.js
@@ -8,6 +8,10 @@ const resendVerificLetter = async (req, res) => {
   if (error) return validationError(error, res);
 
   const userFound = await findUserByEmail(value.email);
+  if (!userFound)
+    return res.status(404).json({
+      message: "No user with this email address found",
+    });
 
   const { verify, email, verificationToken } = userFound;
 
