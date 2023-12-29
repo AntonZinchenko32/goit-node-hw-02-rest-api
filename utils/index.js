43bb321de20e8fccaac306fd426e507a7b2eb067
AntonZinchenko32 <98322005+AntonZinchenko32@users.noreply.github.com>
2023-12-29 03:53:15 +0200

    delete nodemailer from utils (#15)

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,6 @@
 const { createDirIfNotExist } = require("./createDirIfNotExist");
 const { mongoConection } = require("./monogoConection");
 const { saveFileToStorage } = require("./saveFileToStorage");
-const { transporter, emailOptionsBuilder } = require("./nodemailer");
 
 module.exports = {
   mongoConection,
